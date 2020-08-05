b56b806e27471b504bc9240223629103508ea102
Dedenich <sergbrst@tut.by>
2020-08-05 13:57:03 +0300

    Подключит к проекту тестовые движки

---
+++ src/components/app/app.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+// import PropTypes from 'prop-types';
+
+const App = () => <div className="wrapper">
+  Мы в App
+</div>;
+
+export default App;
--- src/index.js
+++ src/index.js
@@ -1,2 +1,10 @@
-const bodyElement = document.querySelector(`body`);
-bodyElement.textContent = `Здесь будет копилка на react`;
+import React from "react";
+import ReactDOM from "react-dom";
+
+import App from "./components/app/App.jsx";
+
+
+ReactDOM.render(
+    <App/>,
+    document.getElementById(`root`)
+);
