6a5cc06c2fc8e22a5286858229fb4155b4018499
Dedenich <sergbrst@tut.by>
2020-08-09 11:55:39 +0300

    Обновит тесты на HOC with-form

--- src/hocs/with-form/with-form.test.js
+++ src/hocs/with-form/with-form.test.js
@@ -1,5 +1,9 @@
 import React from 'react';
 import renderer from 'react-test-renderer';
+import {Provider} from 'react-redux';
+
+import {store as mockStore} from '../../mocks/store-mock.js';
+
 import withForm from "./with-form.jsx";
 
 const MockComponent = () => <div></div>;
@@ -9,10 +13,12 @@ const MockComponentWrapped = withForm(MockComponent);
 it(`withForm is rendered correctly`, () => {
   const tree = renderer
   .create(
-      <MockComponentWrapped
-        isAimAdded={false}
-        onCloseButtonClick={jest.fn()}
-      />
+      <Provider store={mockStore}>
+        <MockComponentWrapped
+          isAimAdded={false}
+          onCloseButtonClick={jest.fn()}
+        />
+      </Provider>
   ).toJSON();
 
   expect(tree).toMatchSnapshot();
